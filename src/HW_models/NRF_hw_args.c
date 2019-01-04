d6bb513a5d3aef3a33278f8c3279a47d77bc1af9
Alberto Escolar Piedras <alpi@oticon.com>
2019-01-04 14:00:41 +0100

    cmd line args: Do not reference nfr52_board variables
    To minimize the interface between the HW models and the board,
    let's not reference the command line arguments structure from the
    hw models.
    
    Signed-off-by: Alberto Escolar Piedras <alpi@oticon.com>

--- src/HW_models/NRF_hw_args.c
+++ src/HW_models/NRF_hw_args.c
@@ -19,10 +19,19 @@ void nrf_hw_sub_cmline_set_defaults(nrf_hw_sub_args_t *args){
 extern void trivial_xo_set_toffset(bs_time_t t_off);
 extern void trivial_xo_set_drift(double xo_d);
 
+double nrfhw_start_of;
 void nrf_hw_cmd_starto_found(char * argv, int offset){
+  args_g_hw->start_offset = nrfhw_start_of;
   trivial_xo_set_toffset(args_g_hw->start_offset);
 }
 
+double nrfhw_drift;
 void nrf_hw_cmd_drift_found(char * argv, int offset){
+  args_g_hw->xo_drift = nrfhw_drift;
   trivial_xo_set_drift(args_g_hw->xo_drift);
+}
+
+bool nrfhw_useRealAES;
+void nrf_hw_cmd_useRealAES_found(char * argv, int offset){
+  args_g_hw->useRealAES = nrfhw_useRealAES;
 }
--- src/HW_models/NRF_hw_args.h
+++ src/HW_models/NRF_hw_args.h
@@ -22,14 +22,18 @@ void nrf_hw_sub_cmline_set_defaults(nrf_hw_sub_args_t *ptr);
 int nrf_hw_sub_cmdline_parse(char *argv_i, nrf_hw_sub_args_t *args);
 void nrf_hw_sub_command_line_handle(nrf_hw_sub_args_t *args, double start_time);
 
+extern double nrfhw_start_of;
 void nrf_hw_cmd_starto_found(char * argv, int offset);
+extern double nrfhw_drift;
 void nrf_hw_cmd_drift_found(char * argv, int offset);
+extern bool nrfhw_useRealAES;
+void nrf_hw_cmd_useRealAES_found(char * argv, int offset);
 
 #define _NRF_HW_SUB_CMD_ARG_STRUCT \
-  /*manual,mandatory,switch, option,             name ,   type,  destination,                       callback,                      , description*/ \
-  { false  , false , false, "start_offset" ,  "start_of", 'f', (void*)&args_g->nrf_hw.start_offset,nrf_hw_cmd_starto_found,"Offset in time (at the start of the simulation) of this device. At time 0 of the device, the phy will be at <start_of>"}, \
-  { false  , false , false, "xo_drift" ,      "xo_drift", 'f', (void*)&args_g->nrf_hw.xo_drift,    nrf_hw_cmd_drift_found, "Simple linear model of the XO drift of this device. For ex. for -30ppm set to -30e-6"}, \
-  { false  , false , false, "RealEncryption", "realAES",  'b', (void*)&args_g->nrf_hw.useRealAES,      NULL,  "(0)/1 Use the real AES encryption for the LL or just send everything in plain text (default) (requires the ext_libCryptov1 component)"}
+  /*manual,mandatory,switch, option,             name ,   type,  destination,                callback,                      , description*/ \
+  { false  , false , false, "start_offset" ,  "start_of", 'f', (void*)&nrfhw_start_of, nrf_hw_cmd_starto_found,"Offset in time (at the start of the simulation) of this device. At time 0 of the device, the phy will be at <start_of>"}, \
+  { false  , false , false, "xo_drift" ,      "xo_drift", 'f', (void*)&nrfhw_drift,    nrf_hw_cmd_drift_found, "Simple linear model of the XO drift of this device. For ex. for -30ppm set to -30e-6"}, \
+  { false  , false , false, "RealEncryption", "realAES",  'b', (void*)&nrfhw_useRealAES, nrf_hw_cmd_useRealAES_found, "(0)/1 Use the real AES encryption for the LL or just send everything in plain text (default) (requires the ext_libCryptov1 component)"}
 
 #ifdef __cplusplus
 }
