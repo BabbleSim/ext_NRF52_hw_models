ba4713391d23df6447e5cc74ebd4f28ca171d5f6
ruge <rubin.gerritsen@nordicsemi.no>
2020-05-25 21:38:20 +0200

    temp: Add stubbed NRF_TEMP peripheral
    No functionality implemented.
    
    Signed-off-by: Rubin Gerritsen <rubin.gerritsen@nordicsemi.no>

---
+++ src/HW_models/NRF_TEMP.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef _NRF_HW_MODEL_TEMP_H
+#define _NRF_HW_MODEL_TEMP_H
+
+#include "NRF_regs.h"
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+extern NRF_TEMP_Type NRF_TEMP_regs;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
