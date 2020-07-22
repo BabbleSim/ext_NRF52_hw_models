469782a6fdc527028a06ba0fa7acff52192a39d4
Sletnes Bjørlo, Aurora <aurora.sletnes@nordicsemi.no>
2020-07-22 12:44:32 +0200

    nvmc: Add simple stub of NVMC
    Signed-off-by: Sletnes Bjørlo, Aurora <aurora.sletnes@nordicsemi.no>

---
+++ src/HW_models/NRF_NVMC.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include "NRF_NVMC.h"
+
+/*
+ * This is only a stub of the register interface with no functionality behind
+ */
+
+NRF_NVMC_Type NRF_NVMC_regs = {0};
