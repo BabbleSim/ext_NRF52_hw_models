f86079a7333968f0ba71fe1266e241295a4e9943
Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
2020-08-12 08:02:09 +0200

    nrfx: hal: Add missing functions
    Add nrf_clock_task_address_get(), nrf_clock_event_address_get(),
    and nrf_power_int_enable_get(), to make it possible to compile
    the nrfx_clock and nrfx_power drivers.
    
    Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- src/nrfx/hal/nrf_power.h
+++ src/nrfx/hal/nrf_power.h
@@ -4,4 +4,14 @@
  * SPDX-License-Identifier: Apache-2.0
  *
  */
+#ifndef BS_NRF_POWER_H__
+#define BS_NRF_POWER_H__
+
 #include "../drivers/nrfx_common.h"
+
+NRF_STATIC_INLINE uint32_t nrf_power_int_enable_get(NRF_POWER_Type const * p_reg)
+{
+    return p_reg->INTENSET;
+}
+
+#endif /* BS_NRF_POWER_H__ */
