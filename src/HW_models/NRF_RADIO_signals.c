2acb8fc12b4119398cddae83a278f001cd8ca6d1
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2023-02-24 09:23:22 +0100

    RADIO: Reorder signals to match spec order
    No functional change
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- src/HW_models/NRF_RADIO_signals.c
+++ src/HW_models/NRF_RADIO_signals.c
@@ -20,6 +20,7 @@
 #include "bs_tracing.h"
 
 extern uint32_t NRF_RADIO_INTEN; //interrupt enable
+extern void nrf_radio_fake_task_TRXEN_TIFS();
 
 void nrf_radio_signal_READY(){
   NRF_RADIO_regs.EVENTS_READY = 1;
@@ -36,6 +37,130 @@ void nrf_radio_signal_READY(){
     hw_irq_ctrl_set_irq(RADIO_IRQn);
   }
 }
+
+void nrf_radio_signal_ADDRESS(){
+  NRF_RADIO_regs.EVENTS_ADDRESS = 1;
+  nrf_ppi_event(RADIO_EVENTS_ADDRESS);
+
+  if ( NRF_RADIO_regs.SHORTS & RADIO_SHORTS_ADDRESS_RSSISTART_Msk ) {
+    nrf_radio_tasks_RSSISTART();
+  }
+  if ( NRF_RADIO_regs.SHORTS & RADIO_SHORTS_ADDRESS_BCSTART_Msk ) {
+    nrf_radio_tasks_BCSTART();
+  }
+
+  if ( NRF_RADIO_INTEN & RADIO_INTENSET_ADDRESS_Msk ){
+    hw_irq_ctrl_set_irq(RADIO_IRQn);
+  }
+}
+
+void nrf_radio_signal_PAYLOAD(){
+  NRF_RADIO_regs.EVENTS_PAYLOAD = 1;
+  nrf_ppi_event(RADIO_EVENTS_PAYLOAD);
+
+  if ( NRF_RADIO_INTEN & RADIO_INTENSET_PAYLOAD_Msk ){
+    hw_irq_ctrl_set_irq(RADIO_IRQn);
+  }
+}
+
+void nrf_radio_signal_END(){
+  NRF_RADIO_regs.EVENTS_END = 1;
+  nrf_ppi_event(RADIO_EVENTS_END);
+
+  if ( NRF_RADIO_regs.SHORTS & RADIO_SHORTS_END_DISABLE_Msk ) {
+    nrf_radio_tasks_DISABLE();
+  }
+  if ( NRF_RADIO_regs.SHORTS & RADIO_SHORTS_END_START_Msk ) {
+    nrf_radio_tasks_START();
+  }
+
+  if ( NRF_RADIO_INTEN & RADIO_INTENSET_END_Msk ) {
+    hw_irq_ctrl_set_irq(RADIO_IRQn);
+  }
+}
+
+void nrf_radio_signal_DISABLED(){
+  NRF_RADIO_regs.EVENTS_DISABLED = 1;
+  nrf_ppi_event(RADIO_EVENTS_DISABLED);
+
+  //These 2 are fake shortcuts meant to start a HW timer for the TIFS
+  if ( NRF_RADIO_regs.SHORTS & RADIO_SHORTS_DISABLED_TXEN_Msk ) {
+    nrf_radio_fake_task_TRXEN_TIFS();
+  }
+  if ( NRF_RADIO_regs.SHORTS & RADIO_SHORTS_DISABLED_RXEN_Msk ) {
+    nrf_radio_fake_task_TRXEN_TIFS();
+  }
+
+  if ( NRF_RADIO_regs.SHORTS & RADIO_SHORTS_DISABLED_RSSISTOP_Msk ) {
+    nrf_radio_tasks_RSSISTOP();
+  }
+
+  if ( NRF_RADIO_INTEN & RADIO_INTENSET_DISABLED_Msk ) {
+    hw_irq_ctrl_set_irq(RADIO_IRQn);
+  }
+}
+
+void nrf_radio_signal_DEVMATCH() {
+  NRF_RADIO_regs.EVENTS_DEVMATCH = 1;
+  nrf_ppi_event(RADIO_EVENTS_DEVMATCH);
+
+  if (NRF_RADIO_INTEN & RADIO_INTENSET_DEVMATCH_Msk) {
+    hw_irq_ctrl_set_irq(RADIO_IRQn);
+  }
+}
+
+void nrf_radio_signal_DEVMISS() {
+  NRF_RADIO_regs.EVENTS_DEVMISS = 1;
+  nrf_ppi_event(RADIO_EVENTS_DEVMISS);
+
+  if (NRF_RADIO_INTEN & RADIO_INTENSET_DEVMISS_Msk) {
+    hw_irq_ctrl_set_irq(RADIO_IRQn);
+  }
+}
+
+void nrf_radio_signal_RSSIEND(){
+  NRF_RADIO_regs.EVENTS_RSSIEND = 1;
+  nrf_ppi_event(RADIO_EVENTS_RSSIEND);
+
+  if ( NRF_RADIO_INTEN & RADIO_INTENSET_RSSIEND_Msk ){
+    hw_irq_ctrl_set_irq(RADIO_IRQn);
+  }
+}
+
+void nrf_radio_signal_BCMATCH() {
+  NRF_RADIO_regs.EVENTS_BCMATCH = 1;
+  nrf_ppi_event(RADIO_EVENTS_BCMATCH);
+
+  if (NRF_RADIO_INTEN & RADIO_INTENSET_BCMATCH_Msk) {
+    hw_irq_ctrl_set_irq(RADIO_IRQn);
+  }
+}
+
+void nrf_radio_signal_CRCOK(){
+  NRF_RADIO_regs.EVENTS_CRCOK = 1;
+  nrf_ppi_event(RADIO_EVENTS_CRCOK);
+
+  if ( NRF_RADIO_INTEN & RADIO_INTENSET_CRCOK_Msk ) {
+    hw_irq_ctrl_set_irq(RADIO_IRQn);
+  }
+}
+
+void nrf_radio_signal_CRCERROR(){
+  NRF_RADIO_regs.EVENTS_CRCERROR = 1;
+  nrf_ppi_event(RADIO_EVENTS_CRCERROR);
+
+  if ( NRF_RADIO_INTEN & RADIO_INTENSET_CRCERROR_Msk ) {
+    hw_irq_ctrl_set_irq(RADIO_IRQn);
+  }
+}
+
+//void nrf_radio_signal_FRAMESTART();
+//void nrf_radio_signal_EDEND();
+//void nrf_radio_signal_EDSTOPPED();
+//void nrf_radio_signal_CCAIDLE();
+//void nrf_radio_signal_CCABUSY();
+//void nrf_radio_signal_CCASTOPPED();
+//void nrf_radio_signal_RATEBOOST();
 
 void nrf_radio_signal_TXREADY(){
   NRF_RADIO_regs.EVENTS_TXREADY = 1;
@@ -66,123 +191,7 @@ void nrf_radio_signal_RXREADY(){
   }
 }
 
-extern void nrf_radio_fake_task_TRXEN_TIFS();
-
-void nrf_radio_signal_DISABLED(){
-  NRF_RADIO_regs.EVENTS_DISABLED = 1;
-  nrf_ppi_event(RADIO_EVENTS_DISABLED);
-
-  //These 2 are fake shortcuts meant to start a HW timer for the TIFS
-  if ( NRF_RADIO_regs.SHORTS & RADIO_SHORTS_DISABLED_TXEN_Msk ) {
-    nrf_radio_fake_task_TRXEN_TIFS();
-  }
-  if ( NRF_RADIO_regs.SHORTS & RADIO_SHORTS_DISABLED_RXEN_Msk ) {
-    nrf_radio_fake_task_TRXEN_TIFS();
-  }
-
-  if ( NRF_RADIO_regs.SHORTS & RADIO_SHORTS_DISABLED_RSSISTOP_Msk ) {
-    nrf_radio_tasks_RSSISTOP();
-  }
-
-  if ( NRF_RADIO_INTEN & RADIO_INTENSET_DISABLED_Msk ) {
-    hw_irq_ctrl_set_irq(RADIO_IRQn);
-  }
-}
-
-void nrf_radio_signal_RSSIEND(){
-  NRF_RADIO_regs.EVENTS_RSSIEND = 1;
-  nrf_ppi_event(RADIO_EVENTS_RSSIEND);
-
-  if ( NRF_RADIO_INTEN & RADIO_INTENSET_RSSIEND_Msk ){
-    hw_irq_ctrl_set_irq(RADIO_IRQn);
-  }
-}
-
-void nrf_radio_signal_ADDRESS(){
-  NRF_RADIO_regs.EVENTS_ADDRESS = 1;
-  nrf_ppi_event(RADIO_EVENTS_ADDRESS);
-
-  if ( NRF_RADIO_regs.SHORTS & RADIO_SHORTS_ADDRESS_RSSISTART_Msk ) {
-    nrf_radio_tasks_RSSISTART();
-  }
-  if ( NRF_RADIO_regs.SHORTS & RADIO_SHORTS_ADDRESS_BCSTART_Msk ) {
-    nrf_radio_tasks_BCSTART();
-  }
-
-  if ( NRF_RADIO_INTEN & RADIO_INTENSET_ADDRESS_Msk ){
-    hw_irq_ctrl_set_irq(RADIO_IRQn);
-  }
-}
-
-void nrf_radio_signal_PAYLOAD(){
-  NRF_RADIO_regs.EVENTS_PAYLOAD = 1;
-  nrf_ppi_event(RADIO_EVENTS_PAYLOAD);
-
-  if ( NRF_RADIO_INTEN & RADIO_INTENSET_PAYLOAD_Msk ){
-    hw_irq_ctrl_set_irq(RADIO_IRQn);
-  }
-}
-
-void nrf_radio_signal_CRCOK(){
-  NRF_RADIO_regs.EVENTS_CRCOK = 1;
-  nrf_ppi_event(RADIO_EVENTS_CRCOK);
-
-  if ( NRF_RADIO_INTEN & RADIO_INTENSET_CRCOK_Msk ) {
-    hw_irq_ctrl_set_irq(RADIO_IRQn);
-  }
-}
-
-void nrf_radio_signal_CRCERROR(){
-  NRF_RADIO_regs.EVENTS_CRCERROR = 1;
-  nrf_ppi_event(RADIO_EVENTS_CRCERROR);
-
-  if ( NRF_RADIO_INTEN & RADIO_INTENSET_CRCERROR_Msk ) {
-    hw_irq_ctrl_set_irq(RADIO_IRQn);
-  }
-}
-
-void nrf_radio_signal_END(){
-  NRF_RADIO_regs.EVENTS_END = 1;
-  nrf_ppi_event(RADIO_EVENTS_END);
-
-  if ( NRF_RADIO_regs.SHORTS & RADIO_SHORTS_END_DISABLE_Msk ) {
-    nrf_radio_tasks_DISABLE();
-  }
-  if ( NRF_RADIO_regs.SHORTS & RADIO_SHORTS_END_START_Msk ) {
-    nrf_radio_tasks_START();
-  }
-
-  if ( NRF_RADIO_INTEN & RADIO_INTENSET_END_Msk ) {
-    hw_irq_ctrl_set_irq(RADIO_IRQn);
-  }
-}
-
-void nrf_radio_signal_DEVMATCH() {
-  NRF_RADIO_regs.EVENTS_DEVMATCH = 1;
-  nrf_ppi_event(RADIO_EVENTS_DEVMATCH);
-
-  if (NRF_RADIO_INTEN & RADIO_INTENSET_DEVMATCH_Msk) {
-    hw_irq_ctrl_set_irq(RADIO_IRQn);
-  }
-}
-
-void nrf_radio_signal_DEVMISS() {
-  NRF_RADIO_regs.EVENTS_DEVMISS = 1;
-  nrf_ppi_event(RADIO_EVENTS_DEVMISS);
-
-  if (NRF_RADIO_INTEN & RADIO_INTENSET_DEVMISS_Msk) {
-    hw_irq_ctrl_set_irq(RADIO_IRQn);
-  }
-}
-
-void nrf_radio_signal_BCMATCH() {
-  NRF_RADIO_regs.EVENTS_BCMATCH = 1;
-  nrf_ppi_event(RADIO_EVENTS_BCMATCH);
-
-  if (NRF_RADIO_INTEN & RADIO_INTENSET_BCMATCH_Msk) {
-    hw_irq_ctrl_set_irq(RADIO_IRQn);
-  }
-}
+//void nrf_radio_signal_MHRMATCH();
 
 void nrf_radio_signal_SYNC() {
   NRF_RADIO_regs.EVENTS_SYNC = 1;
@@ -208,3 +217,5 @@ void nrf_radio_signal_PHYEND(){
     hw_irq_ctrl_set_irq(RADIO_IRQn);
   }
 }
+
+//void nrf_radio_signal_CTEPRESENT();
--- src/HW_models/NRF_RADIO_signals.h
+++ src/HW_models/NRF_RADIO_signals.h
@@ -12,21 +12,30 @@
 extern "C"{
 #endif
 
-void nrf_radio_signal_DISABLED();
 void nrf_radio_signal_READY();
-void nrf_radio_signal_TXREADY();
-void nrf_radio_signal_RXREADY();
-void nrf_radio_signal_RSSIEND();
 void nrf_radio_signal_ADDRESS();
 void nrf_radio_signal_PAYLOAD();
-void nrf_radio_signal_CRCOK();
-void nrf_radio_signal_CRCERROR();
 void nrf_radio_signal_END();
+void nrf_radio_signal_DISABLED();
 void nrf_radio_signal_DEVMATCH();
 void nrf_radio_signal_DEVMISS();
+void nrf_radio_signal_RSSIEND();
 void nrf_radio_signal_BCMATCH();
+void nrf_radio_signal_CRCOK();
+void nrf_radio_signal_CRCERROR();
+//void nrf_radio_signal_FRAMESTART();
+//void nrf_radio_signal_EDEND();
+//void nrf_radio_signal_EDSTOPPED();
+//void nrf_radio_signal_CCAIDLE();
+//void nrf_radio_signal_CCABUSY();
+//void nrf_radio_signal_CCASTOPPED();
+//void nrf_radio_signal_RATEBOOST();
+void nrf_radio_signal_TXREADY();
+void nrf_radio_signal_RXREADY();
+//void nrf_radio_signal_MHRMATCH();
 void nrf_radio_signal_SYNC();
 void nrf_radio_signal_PHYEND();
+//void nrf_radio_signal_CTEPRESENT();
 
 #ifdef __cplusplus
 }
