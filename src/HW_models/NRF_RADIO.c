BabbleSim/ext_NRF52_hw_models

commit cee41373eb0bb720a370586de1ee5eb693e62c95

RADIO: Improve T_IFS support, and fix for low latency mode
a) Fix for the HW automatically generated TIFS:
One condition which disables this HW feature was missing,
which lead to it being enabled when it shouldnt in some cases.
=> Fix it

b) Fix the DISABLED=>[TR]XEN shortcuts when automatic HW IFS is not used.
The shortcuts were missing in this case.

c) Postpone Tx request to the Phy until the last moment:
That is, do the request Tx_chain_delay later.
As otherwise, if another (pending) HW event was scheduled at the exact
same time as the Tx start it would cause the request to have an abort
reevaluation before the Tx start, which is a violation of the
protocol to the phy.
This issue was triggered, in low latency mode, where a TIMER event
was used to trigger the Tx start task, but could have been triggered
by other concurrent HW events.

Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

src/HW_models/NRF_RADIO.c
@@ -262,7 +262,6 @@ void nrf_radio_tasks_TXEN() {
         radio_state);
     return;
   }
-  TIFS_state = TIFS_DISABLE;
   radio_state = RAD_TXRU;
   NRF_RADIO_regs.STATE = RAD_TXRU;
 
@@ -300,7 +299,9 @@ static void abort_if_needed(){
 
 void nrf_radio_tasks_START () {
   if ( radio_state == RAD_TXIDLE ) {
-    start_Tx();
+    bs_time_t Tx_start_time = tm_get_abs_time() + nrfra_timings_get_TX_chain_delay();
+    radio_state = RAD_TXSTARTING;
+    nrfra_set_Timer_RADIO(Tx_start_time);
   } else if ( radio_state == RAD_RXIDLE ) {
     start_Rx();
   } else {
@@ -495,6 +496,7 @@ void nrf_radio_fake_task_TRXEN_TIFS(){
 void maybe_prepare_TIFS(bool Tx_Not_Rx){
   bs_time_t delta;
   if ( !nrfra_is_HW_TIFS_enabled() ) {
+    TIFS_state = TIFS_DISABLE;
     return;
   }
   if ( NRF_RADIO_regs.SHORTS & RADIO_SHORTS_DISABLED_TXEN_Msk ){
@@ -509,7 +511,7 @@ void maybe_prepare_TIFS(bool Tx_Not_Rx){
     delta = NRF_RADIO_regs.TIFS - nrfra_timings_get_Rx_chain_delay() - nrfra_timings_get_TX_chain_delay() - nrfra_timings_get_rampup_time(1, 1) + 1;
   }
   Timer_TIFS = tm_get_hw_time() + delta;
-  TIFS_state = TIFS_WAITING_FOR_DISABLE;
+  TIFS_state = TIFS_WAITING_FOR_DISABLE; /* In Timer_TIFS we will trigger a TxEN or RxEN */
 }
 
 /**
@@ -531,6 +533,9 @@ void nrf_radio_timer_triggered(){
     nrfra_set_Timer_RADIO(TIME_NEVER);
     nrf_radio_signal_READY();
     nrf_radio_signal_RXREADY();
+  } else if ( radio_state == RAD_TXSTARTING ){
+    nrfra_set_Timer_RADIO(TIME_NEVER);
+    start_Tx();
   } else if ( radio_state == RAD_TX ){
     if ( radio_sub_state == TX_WAIT_FOR_ADDRESS_END ){
       radio_sub_state = TX_WAIT_FOR_PAYLOAD_END;
@@ -768,7 +773,7 @@ static void start_Tx(){
   int ret = p2G4_dev_req_txv2_nc_b(&tx_status.tx_req, tx_buf,  &tx_status.tx_resp);
   handle_Tx_response(ret);
 
-  tx_status.ADDRESS_end_time = tm_get_hw_time() + (bs_time_t)((preamble_len*8 + address_len*8)/bits_per_us);
+  tx_status.ADDRESS_end_time = tm_get_hw_time() + (bs_time_t)((preamble_len*8 + address_len*8)/bits_per_us) - nrfra_timings_get_TX_chain_delay();
   tx_status.PAYLOAD_end_time = tx_status.ADDRESS_end_time + (bs_time_t)(8*(header_len + payload_len)/bits_per_us);
   tx_status.CRC_end_time = tx_status.PAYLOAD_end_time + (bs_time_t)(crc_len*8/bits_per_us);
 

src/HW_models/NRF_RADIO_priv.h
@@ -25,6 +25,7 @@ typedef enum { //Note: This should match the real RADIO state values in the STAT
 
   RAD_TXRU = 9, //The radio is ramping up and preparing for transmission
   RAD_TXIDLE, //The radio is ready for transmission to start
+  RAD_TXSTARTING, //The radio is starting to Tx (it will be in this state for TxChainDelay)
   RAD_TX, //The radio is transmitting a packet
   RAD_TXDISABLE, //The radio is disabling the transmitter
 

src/HW_models/NRF_RADIO_signals.c
@@ -15,6 +15,7 @@
  */
 
 #include "NRF_RADIO.h"
+#include "NRF_RADIO_utils.h"
 #include "NRF_PPI.h"
 #include "irq_ctrl.h"
 #include "bs_tracing.h"
@@ -83,12 +84,26 @@ void nrf_radio_signal_DISABLED(){
   NRF_RADIO_regs.EVENTS_DISABLED = 1;
   nrf_ppi_event(RADIO_EVENTS_DISABLED);
 
-  //These 2 are fake tasks meant to start a HW timer for the TIFS
-  if ( NRF_RADIO_regs.SHORTS & RADIO_SHORTS_DISABLED_TXEN_Msk ) {
-    nrf_radio_fake_task_TRXEN_TIFS();
-  }
-  if ( NRF_RADIO_regs.SHORTS & RADIO_SHORTS_DISABLED_RXEN_Msk ) {
-    nrf_radio_fake_task_TRXEN_TIFS();
+  /*
+   * Everything indicates that, when the HW TIFS is enabled
+   * what happens is that these DISABLED_[TR]XEN shorts are
+   * effectively disabled and the tasks_[TR]XEN is instead
+   * triggered when a HW counter/timer triggers a bit later
+   */
+  if (nrfra_is_HW_TIFS_enabled()) {
+     /* This is a fake task meant to start a HW timer for the TIFS
+      * which has effect only if the TIFS was enabled.
+      * In that case, the TXEN or RXEN will be triggered in a small
+      * while (as per the TIFS configuration/Timer_TIFS), instead of right now */
+      nrf_radio_fake_task_TRXEN_TIFS();
+  } else {
+    /* Otherwise the normal TXEN/RXEN shortcuts apply */
+    if ( NRF_RADIO_regs.SHORTS & RADIO_SHORTS_DISABLED_TXEN_Msk ) {
+      nrf_radio_tasks_TXEN();
+    }
+    if ( NRF_RADIO_regs.SHORTS & RADIO_SHORTS_DISABLED_RXEN_Msk ) {
+      nrf_radio_tasks_RXEN();
+    }
   }
 
   if ( NRF_RADIO_regs.SHORTS & RADIO_SHORTS_DISABLED_RSSISTOP_Msk ) {

src/HW_models/NRF_RADIO_utils.c
@@ -170,7 +170,8 @@ int nrfra_is_HW_TIFS_enabled() {
   if ( ( NRF_RADIO_regs.SHORTS & RADIO_SHORTS_END_DISABLE_Msk )
       && ( ( NRF_RADIO_regs.SHORTS & RADIO_SHORTS_DISABLED_RXEN_Msk )
           || ( NRF_RADIO_regs.SHORTS & RADIO_SHORTS_DISABLED_TXEN_Msk ) )
-  ){
+      && ( (NRF_RADIO_regs.MODECNF0 & RADIO_MODECNF0_RU_Msk) == 0) )
+  {
     return 1;
   }
   return 0;
@@ -305,7 +306,7 @@ void nrfra_prep_tx_request(p2G4_txv2_t *tx_req, uint packet_size, bs_time_t pack
   tx_req->packet_size  = packet_size; //Not including preamble or address
 
   {
-    bs_time_t tx_start_time = tm_get_abs_time() + nrfra_timings_get_TX_chain_delay();
+    bs_time_t tx_start_time = tm_get_abs_time();
     tx_req->start_tx_time = hwll_phy_time_from_dev(tx_start_time);
     tx_req->start_packet_time = tx_req->start_tx_time ;
     tx_req->end_tx_time = tx_req->start_tx_time + packet_duration;

commit 0a7f846b0ad38dd7355e5728e7ba77af91c5f848

RADIO: Bugfix if DISABLE or STOP during first us of TxStart
If a DISABLE or STOP was received during the first TxChainDelay (1us)
after a Tx START, the task would have been missed.
Fix it.

The issue was introduced in cee41373eb0bb720a370586de1ee5eb693e62c95
where the actual Tx start was delayed by TxChainDelay.

Also set already the RADIO STATE register to TX
as soon as the TX START is triggered instead of postponing it
by that microsecond.

Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

src/HW_models/NRF_RADIO.c
@@ -301,6 +301,7 @@ void nrf_radio_tasks_START () {
   if ( radio_state == RAD_TXIDLE ) {
     bs_time_t Tx_start_time = tm_get_abs_time() + nrfra_timings_get_TX_chain_delay();
     radio_state = RAD_TXSTARTING;
+    NRF_RADIO_regs.STATE = RAD_TX;
     nrfra_set_Timer_RADIO(Tx_start_time);
   } else if ( radio_state == RAD_RXIDLE ) {
     start_Rx();
@@ -366,8 +367,10 @@ void nrf_radio_tasks_EDSTOP() {
 void nrf_radio_tasks_STOP(){
   nrf_radio_stop_bit_counter();
 
-  if ( radio_state == RAD_TX ){
-    abort_if_needed();
+  if ((radio_state == RAD_TX) || (radio_state == RAD_TXSTARTING)) {
+    if (radio_state == RAD_TX) {
+      abort_if_needed();
+    }
     radio_state = RAD_TXIDLE;
     NRF_RADIO_regs.STATE = RAD_TXIDLE;
     nrfra_set_Timer_RADIO(TIME_NEVER);
@@ -398,9 +401,11 @@ void nrf_radio_tasks_STOP(){
 void nrf_radio_tasks_DISABLE() {
   nrf_radio_stop_bit_counter();
 
-  if ( radio_state == RAD_TX ){
-    abort_if_needed();
-    radio_state = RAD_TXIDLE; //Momentary (will be changedin the if below)
+  if ((radio_state == RAD_TX) || (radio_state == RAD_TXSTARTING)) {
+    if (radio_state == RAD_TX) {
+      abort_if_needed();
+    }
+    radio_state = RAD_TXIDLE; //Momentary (will be changed in the if below)
     NRF_RADIO_regs.STATE = RAD_TXIDLE;
   } else if ( radio_state == RAD_RX ){
     abort_if_needed();