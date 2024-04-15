ed8f9b7e97e7ca9d6644def1efd4362977f31d94
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2024-04-15 13:41:07 +0200

    Make: Provide a reasonable default for BSIM_COMPONENTS_PATH
    In case these HW models have not been fetched in the
    ${BSIM_COMPONENTS_PATH} folder, but BSIM_BASE_PATH has been set.
    
    Also describe in a comment the expectation for these 2 env
    variables.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- make_inc/pre.mk
+++ make_inc/pre.mk
@@ -2,7 +2,14 @@
 # Copyright 2023 Nordic Semiconductor ASA
 # SPDX-License-Identifier: Apache-2.0
 
+# BSIM_BASE_PATH must point to the bsim base component folder (typically placed directly in 
+# bsim/components).
+# And BSIM_COMPONENTS_PATH must point to the bsim/components folder.
+# if these HW models were fetched inside the bsim/components/ folder, it is very likely we will just
+# find them one level up. Otherwise these must be properly set in the environment:
 BSIM_BASE_PATH?=$(abspath ../ )
+BSIM_COMPONENTS_PATH?=${BSIM_BASE_PATH}
+
 include ${BSIM_BASE_PATH}/common/pre.make.inc
 
 2G4_libPhyComv1_COMP_PATH?=$(abspath ${BSIM_COMPONENTS_PATH}/ext_2G4_libPhyComv1)
