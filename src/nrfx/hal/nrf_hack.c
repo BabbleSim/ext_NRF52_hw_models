086fe1e56a207f99a3e52462cb254fd8f5c9ae36
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2024-04-22 14:24:08 +0200

    hal: hack: Add initial nRF54L15 support
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- src/nrfx/hal/nrf_hack.c
+++ src/nrfx/hal/nrf_hack.c
@@ -30,6 +30,22 @@
 #include "hal/nrf_ipc.h"
 #include "hal/nrf_rtc.h"
 #include "hal/nrf_timer.h"
+#elif defined(NRF54L15_XXAA)
+#include "hal/nrf_aar.h"
+#include "hal/nrf_ccm.h"
+#include "hal/nrf_clock.h"
+#include "hal/nrf_ecb.h"
+#include "hal/nrf_egu.h"
+#include "hal/nrf_dppi.h"
+#include "hal/nrf_gpiote.h"
+#include "hal/nrf_grtc.h"
+//#include "hal/nrf_pdm.h" //Pending support in real HAL
+#include "hal/nrf_ppib.h"
+#include "hal/nrf_pwm.h"
+#include "hal/nrf_radio.h"
+#include "hal/nrf_rtc.h"
+#include "hal/nrf_timer.h"
+#include "hal/nrf_temp.h"
 #endif
 
 #include "bs_tracing.h"
@@ -55,6 +71,7 @@ static bool nrf_dppi_hack_is_task(int task_event, void *p_reg) {
    *  [0x080..0x100) are subscribe registers
    *  [0x100..0x180) are events
    *  [0x180..0x200) are publish registers
+   *  Note: For the nrf54 RADIO, each group is twice as big, but tasks are still < 0x100
    */
   if (task_event < 0x100) {
     return true;
@@ -148,6 +165,87 @@ static void nrf_dppi_hack_get_task_from_ptr(void *task_reg,
   } else IF_PER(I2S0, , _NS, dppi)*/
   } else IF_PER(IPC, , _NS, ipc)
   /* QSPI, NFCT */
+  } else {
+    bs_trace_error_time_line("Tried to look for a task register not known to these HW models\n");
+    return; /* unreachable */
+  }
+#elif defined(NRF54L15_XXAA)
+  /*IF_PER(SPU, 00, _S, spu)
+  } else IF_PER(MPC, 00, _S, mpc)
+  } else*/ IF_PER(DPPIC, 00, _S, dppi)
+  //} else IF_PER(PPIB, 00, _S, ppib)
+  //} else IF_PER(KMU, , _S, kmu)
+  //} else IF_PER(AAR, 00, _S, aar)
+  //} else IF_PER(CCM, 00, _S, ccm)
+  //} else IF_PER(ECB, 00, _S, ecb)
+  //} else IF_PER(CRCEN, , _S, cracen)
+  //} else IF_PER(SPIM, 00, _S, spi)
+  //} else IF_PER(UARTE, 00, _S, uarte)
+  //} else IF_PER(GLITCHDET, , _S, glitchdet)
+  //} else IF_PER(RRAMC, , _S, rramc)
+  //} else IF_PER(VPR, 00 , _S, vpr)
+  //} else IF_PER(CTRLAP, , _S, ctrlap)
+  } else IF_PER(TIMER, 00, _S, timer)
+  //} else IF_PER(SPU, 10, _S, spu)
+  } else IF_PER(DPPIC, 10, _S, dppi)
+  //} else IF_PER(PPIB, 10, _S, ppib)
+  //} else IF_PER(PPIB, 11, _S, ppib)
+  } else IF_PER(TIMER, 10, _S, timer)
+  } else IF_PER(RTC, 10, _S, rtc)
+  } else IF_PER(EGU, 10, _S, egu)
+  } else IF_PER(RADIO, , _S, radio)
+  //} else IF_PER(SPU, 20, _S, spu)
+  } else IF_PER(DPPIC, 20, _S, dppi)
+  //} else IF_PER(PPIB, 20, _S, ppib)
+  //} else IF_PER(PPIB, 21, _S, ppib)
+  //} else IF_PER(PPIB, 22, _S, ppib)
+  //} else IF_PER(SPIM, 20, _S, spi)
+  //} else IF_PER(TWIM, 20, _S, twi)
+  //} else IF_PER(UARTE, 20, _S, uarte)
+  //} else IF_PER(SPIM, 21, _S, spi)
+  //} else IF_PER(TWIM, 21, _S, twi)
+  //} else IF_PER(UARTE, 21, _S, uarte)
+  //} else IF_PER(SPIM, 22, _S, spi)
+  //} else IF_PER(TWIM, 22, _S, twi)
+  //} else IF_PER(UARTE, 22, _S, uarte)
+  } else IF_PER(EGU, 20, _S, egu)
+  } else IF_PER(TIMER, 20, _S, timer)
+  } else IF_PER(TIMER, 21, _S, timer)
+  } else IF_PER(TIMER, 22, _S, timer)
+  } else IF_PER(TIMER, 23, _S, timer)
+  } else IF_PER(TIMER, 24, _S, timer)
+  //} else IF_PER(MEMCONF, , _S, memconf)
+  //} else IF_PER(PDM, 20, _S, pdm)
+  //} else IF_PER(PDM, 21, _S, pdm)
+  //} else IF_PER(PWM, 20, _S, pwm)
+  //} else IF_PER(PWM, 21, _S, pwm)
+  //} else IF_PER(PWM, 22, _S, pwm)
+  //} else IF_PER(SAADC, , _S, saadc)
+  //} else IF_PER(NFCT, , _S, nfct)
+  } else IF_PER(TEMP, , _S, temp)
+  //} else IF_PER(GPIOTE, 20, _S, gpiote)
+  //} else IF_PER(TAMPC, , _S, tamp)
+  //} else IF_PER(I2S, 20, _S, i2s)
+  //} else IF_PER(QDEC, 20, _S, qdec)
+  //} else IF_PER(QDEC, 21, _S, qdec)
+  } else IF_PER(GRTC, , _S, grtc)
+  //} else IF_PER(SPU, 30, _S, spu)
+  } else IF_PER(DPPIC, 30, _S, dppi)
+  //} else IF_PER(PPIB, 30, _S, ppib)
+  //} else IF_PER(SPIM, 30, _S, spi)
+  //} else IF_PER(TWIM, 30, _S, twi)
+  //} else IF_PER(UARTE, 30, _S, uarte)
+  } else IF_PER(RTC, 30, _S, rtc)
+  //} else IF_PER(COMP, , _S, comp)
+  //} else IF_PER(LPCOMP, , _S, lpcomp)
+  //} else IF_PER(WDT, 30, _S, wdt)
+  //} else IF_PER(WDT, 31, _S, wdt)
+  //} else IF_PER(GPIOTE, 30, _S, gpiote)
+  } else IF_PER(CLOCK, , _NS, clock)
+  //} else IF_PER(POWER, , _NS, power)
+  //} else IF_PER(RESET, , _NS, reset)
+  //} else IF_PER(OSCILLARTORS, , _NS, oscillators)
+  //} else IF_PER(REGULATORS, , _NS, regulators)
   } else {
     bs_trace_error_time_line("Tried to look for a task register not known to these HW models\n");
     return; /* unreachable */
